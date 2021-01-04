48a9e0ae7174e90b27272e3370eee1c4274782be
banyarthein <banyarthein@gmail.com>
2021-01-04 21:06:31 +0800

    ticketing

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,12 @@ import {json} from 'body-parser';
 const app = express();
 app.use(json());
 
+
+
+app.get('/api/users/currentuser', (request, response)=> {
+    response.send('Hey Thar Khit');
+});
+
 app.listen(3000, () =>{
-  console.log('Listening on port 3000!');
+  console.log('Listening on port 3000! yayyy!!!');
 })
