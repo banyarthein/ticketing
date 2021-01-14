38796e18fb3235cdd8bb424b69f60fa3982fa302
banyarthein <banyar.lanwork@gmail.com>
2021-01-14 21:13:11 +0800

    using JWT for Login Token

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
 import { DatabaseConnectionError } from '../errors/database-connection-error';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
@@ -35,6 +37,21 @@ router.post('/api/users/signup',
 
         const user = User.build({ email, password });
         await user.save();
+
+        //Generate JWT
+        const userJwt = jwt.sign({
+            id: user.id,
+            email: user.email
+        }, 'ticketing');
+
+        console.log(userJwt);
+
+        //Store it on session object
+        req.session = {
+            jwt: userJwt
+        };
+
+        console.log(req.session);
 
         res.status(201).send({ user });
         // new User ({email, password})
