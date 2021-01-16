0ea6ef0ca7dd5016952aae6f4ea2a2ceb47470da
banyarthein <banyar.lanwork@gmail.com>
2021-01-16 20:54:27 +0800

    Sign Out and other

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -1,7 +1,8 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 const router = express.Router();
 
-router.post('/api/users/signout', (request, response) => {
-    response.send("Hi there!");
+router.post('/api/users/signout', (req: Request, res: Response) => {
+    req.session = null;
+    res.send({ currentUser: "logged out" });
 });
 export { router as signoutRouter };
