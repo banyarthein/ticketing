73749e302ba487c1d16b0b259c53c801190332bc
banyarthein <banyarthein@gmail.com>
2021-02-07 17:08:10 +0800

    code with some err

--- tickets/src/routes/__tests__/new.test.ts
+++ tickets/src/routes/__tests__/new.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import {app} from "../../app";
+import { Ticket } from '../../models/tickets';
 
 
 
@@ -65,6 +66,10 @@ it("returns an error if an invalid price is provided", async() => {
 });
 
 it("creates a ticket with valid inputs", async() => {
+
+    let tickets = await Ticket.find({});
+    expect (tickets.length).toEqual(0);
+
     await request (app)
     .post('/api/tickets')
     .set('Cookie', global.signin())
@@ -73,4 +78,10 @@ it("creates a ticket with valid inputs", async() => {
         price: 20
     })
     .expect(201);
+
+    tickets = await Ticket.find({});
+    expect(tickets[0].price).toEqual(1);
+    expect(Ticket.length).toEqual(1);
+
+
 });
