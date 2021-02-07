cd7f583eb3b55c0286dd9167e718a50b154241fa
banyarthein <banyarthein@gmail.com>
2021-02-07 19:16:57 +0800

    issue fixed for show test

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -4,7 +4,7 @@ import { json } from "body-parser";
 import cookieSession from "cookie-session";
 import {currentUser, errorHandler, NotFoundError} from "@ticketingbnt/common";
 import {createTicketRouter} from "./routes/new";
-import {ShowTicketRouter} from "./routes/show";
+import {showTicketRouter} from "./routes/show";
 
 const app = express();
 app.set("trust proxy", true);
@@ -17,7 +17,7 @@ app.use(
 );
 app.use(currentUser);
 app.use(createTicketRouter);
-app.use(ShowTicketRouter);
+app.use(showTicketRouter);
 
 app.all("*", async (req, res) => {
   throw new NotFoundError();
--- tickets/src/routes/show.ts
+++ tickets/src/routes/show.ts
@@ -1,17 +1,17 @@
-import { NotFoundError } from '@ticketingbnt/common';
-import express, {Request, Response } from 'express';
-import { Ticket} from '../models/tickets';
+import { NotFoundError } from "@ticketingbnt/common";
+import express, { Request, Response } from "express";
+import { Ticket } from  "../models/tickets";
 
 const router = express.Router();
 
-router.get("/api/tivckets/:id", async(req: Request, res: Response) => {
-    const ticket = await Ticket.findById(req.params.id);
+router.get("/api/tickets/:id", async (req: Request, res: Response) => {
+  const ticket = await Ticket.findById(req.params.id);
 
-    if(!ticket) {
-        throw new NotFoundError();
-    }
+  if (!ticket) {
+    throw new NotFoundError();
+  }
 
-    res.send(ticket);
+  res.send(ticket);
 });
 
-export {router as ShowTicketRouter};
+export { router as showTicketRouter };
