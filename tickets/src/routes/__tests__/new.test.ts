aaa66dfd61407b716598bdc9fcad429d07331e05
banyarthein <banyarthein@gmail.com>
2021-02-07 16:35:38 +0800

    added new test methods

--- tickets/src/routes/__tests__/new.test.ts
+++ tickets/src/routes/__tests__/new.test.ts
@@ -18,20 +18,56 @@ it("return a status other than 401 if the user is signed in", async() => {
 
     const response = await  request(app)
                         .post("/api/tickets")
+                        .set('Cookie', global.signin())
                         .send({});
     expect (response.status).not.toEqual(404);
 
 });
 
 
-it("returns an error if an invalid title is provided", async() => {});
+it("returns an error if an invalid title is provided", async() => {
+    await request (app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title: '',
+            price: 10
+        })
+        .expect(400);
 
-it("returns an error if an invalid title is provided", async() => {});
+        await request (app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            price: 10
+        })
+        .expect(400);
+                
+});
 
-it("returns an error if an invalid title is provided", async() => {});
+it("returns an error if an invalid price is provided", async() => {
+    await request (app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+        title: 'this is valid title',
+        price: -10
+    })
+    .expect(400);
 
-it("returns an error if an invalid title is provided", async() => {});
+    await request (app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+        title: 'valid title'
+    })
+    .expect(400);
+});
 
-it("returns an error if an invalid title is provided", async() => {});
+it("returns an error if an invalid aaa is provided", async() => {});
+
+it("returns an error if an invalid bbb is provided", async() => {});
+
+it("returns an error if an invalid ccc is provided", async() => {});
 
 it("creates a ticket with valid inputs", async() => {});
--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -2,11 +2,12 @@ import request from "supertest";
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
 import { app } from "../app";
+import jwt from 'jsonwebtoken';
 
 declare global {
   namespace NodeJS {
     interface Global {
-      signin(): Promise<string[]>;
+      signin(): string[];
     }
   }
 }
@@ -38,18 +39,27 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-global.signin = async () => {
-  const email = "test@test.com";
-  const password = "password";
+global.signin = () => {
 
-  const response = await request(app)
-    .post("/api/users/signup")
-    .send({
-      email,
-      password,
-    })
-    .expect(201);
+  //Build a JSONWebToken payload. {id, email}
+  const payload = {
+    id: '1jk23j123l',
+    email: 'tharkhit@bz.com'
+  }
 
-  const cookie = response.get("Set-Cookie");
-  return cookie;
+  //Create the JWT!
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+
+  //Build session Object. {jwt: MY_JWT}
+  const session = {jwt: token};
+
+  //Turn that session into JSON
+  const sessionJSON = JSON.stringify(session);
+
+  // Take JSON and endoce it as base64
+  const base64 = Buffer.from(sessionJSON).toString('base64');
+
+  // return a string that has the cookies with the encoded data.
+  return [`express:sess=${base64}`];    
 };
