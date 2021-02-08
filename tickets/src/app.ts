ebd3745bc01e875a636ed68e2ca09d96d85edc93
banyarthein <banyarthein@gmail.com>
2021-02-08 21:53:10 +0800

    added Index Tickets

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -5,6 +5,7 @@ import cookieSession from "cookie-session";
 import {currentUser, errorHandler, NotFoundError} from "@ticketingbnt/common";
 import {createTicketRouter} from "./routes/new";
 import {showTicketRouter} from "./routes/show";
+import { indexTicketRouter } from "./routes";
 
 const app = express();
 app.set("trust proxy", true);
@@ -17,6 +18,7 @@ app.use(
 );
 app.use(currentUser);
 app.use(createTicketRouter);
+app.use(indexTicketRouter);
 app.use(showTicketRouter);
 
 app.all("*", async (req, res) => {
