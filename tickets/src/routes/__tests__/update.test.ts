00872a9cfd47cf83463ee68c645efd4e4e3f1fce
banyarthein <banyarthein@gmail.com>
2021-02-08 23:05:51 +0800

    update ticket (completd all test methods)

--- tickets/src/routes/__tests__/update.test.ts
+++ tickets/src/routes/__tests__/update.test.ts
@@ -45,7 +45,7 @@ it("returns a 401 if the user does not own the ticket", async () => {
       .put(`/api/tickets/${response.body.id}`)
       .set("Cookie", global.signin())
       .send({
-        title: "new title",
+        title: " title",
         price: 30,
       })
       .expect(401);
@@ -53,11 +53,65 @@ it("returns a 401 if the user does not own the ticket", async () => {
 
 it("returns a 400 if the user provideds an invalid title or price", async() => 
 {
+    const cookie =global.signin();
 
+    const response = await request(app)
+      .post("/api/tickets")
+      .set("Cookie", cookie)
+      .send({
+        title: "abc title",
+        price: 20,
+      });
+  
+    await request(app)
+      .put(`/api/tickets/${response.body.id}`)
+      .set("Cookie", cookie)
+      .send({
+        title: "",
+        price: 30,
+      })
+      .expect(400);
+
+    
+      await request(app)
+      .put(`/api/tickets/${response.body.id}`)
+      .set("Cookie", cookie)
+      .send({
+        title: "This is new",
+        price: -10,
+      })
+      .expect(400);
+  
 });
 
-it("returns a 404 if the provided ID does not exists", async() => 
-{
 
+it("update the ticket if valid input is provided", async() => 
+{
+    const cookie =global.signin();
+
+    const response = await request(app)
+        .post("/api/tickets")
+        .set("Cookie", cookie)
+        .send({
+            title: "This is New Title",
+            price: 100,
+        });
+
+    await request(app)
+        .put(`/api/tickets/${response.body.id}`)
+        .set('Cookie', cookie)
+        .send({
+            title: "This is Updated Title v2",
+            price: 120,
+        });
+
+
+    const ticketResponse = await request(app)
+        .get(`/api/tickets/${response.body.id}`)
+        .send();
+
+    expect(ticketResponse.body.title).toEqual("This is Updated Title v2");
+    expect(ticketResponse.body.price).toEqual(120);
+    
 });
 
