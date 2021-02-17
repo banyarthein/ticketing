1762e12134d9f98b446eaaf683123509b462b52b
banyarthein <banyarthein@gmail.com>
2021-02-17 22:15:42 +0800

    nats publisher and listener test

---
+++ nats-test/src/listener.ts
@@ -0,0 +1,38 @@
+import nats, {Message} from 'node-nats-streaming';
+import {randomBytes} from 'crypto';
+
+console.clear();
+
+const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
+    url: 'http://localhost:4222',
+})
+
+stan.on('connect', () => {
+    console.log("Listener connected to NATS");
+    stan.on("close", () => {
+
+        console.log("Closing down. ... ...");
+        process.exit();
+    })
+
+    const options = stan.subscriptionOptions()
+                        .setManualAckMode(true);
+
+    const subscription = stan.subscribe("ticket:created",
+                                        "order-service-quere-group",
+                                        options);
+
+    subscription.on("message", (msg: Message) => {
+        const data = msg.getData();
+
+        if(typeof data === "string")
+        {
+            console.log(`Received event #${msg.getSequence()}, with data:${data}`)
+        }
+
+        msg.ack();
+    });
+})
+
+process.on('STGINT', () => stan.close());
+process.on('SIGTERM', () => stan.close());
