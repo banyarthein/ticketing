0a629e0dc16b3261620db6ca3a0d403d42af2f73
banyarthein <banyarthein@gmail.com>
2021-02-19 22:07:12 +0800

    making strongly typed

---
+++ nats-test/src/events/ticket-created-listener.ts
@@ -0,0 +1,20 @@
+import {Message, Stan} from 'node-nats-streaming';
+import {Listener} from '../events/base-listener';
+import { Subjects } from './subjects';
+import {TicketCreatedEvent} from "./ticket-created-event";
+
+export class TicketCreatedListener extends Listener<TicketCreatedEvent>{
+    subject:  Subjects.TicketCreated = Subjects.TicketCreated;
+    queueGroupName = "payment-service";
+
+    onMessage(data: TicketCreatedEvent["data"], msg: Message) 
+    {
+        console.log("Event data!", data);
+
+        console.log(data.id);
+        console.log(data.title);
+        console.log(data.price);
+
+        msg.ack();
+    }
+}
