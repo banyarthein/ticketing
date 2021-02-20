d068a91cde57c97f05136bc44a85c956c1d89086
banyarthein <banyarthein@gmail.com>
2021-02-20 14:07:35 +0800

    Bump Version

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -2,6 +2,7 @@ import express, {Request, Response} from 'express';
 import {body} from 'express-validator';
 import {requireAuth, validateRequest} from '@ticketingbnt/common';
 import { Ticket } from '../models/tickets';
+import {TicketCreatedPublisher} from "../events/publishers/ticket-created-publisher";
 
 
 const router = express.Router();
@@ -22,6 +23,12 @@ async (req: Request, res: Response) => {
     })
 
     await ticket.save();
+    new TicketCreatedPublisher(client).publish({
+            id: ticket.id,
+            title: ticket.title,
+            price: ticket.price,
+            userId: ticket.userId,
+        });
 
     res.status(201).send(ticket);
 
