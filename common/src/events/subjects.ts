410d0f5f840ff67d74045fc1a6c760df01c47774
banyarthein <banyarthein@gmail.com>
2021-03-01 22:50:38 +0800

    Bump Version

--- common/src/events/subjects.ts
+++ common/src/events/subjects.ts
@@ -2,5 +2,8 @@ export enum Subjects
 {
     TicketCreated = "ticket:created",
     TicketUpdated = "ticket:updated",
+
+    OrderCreated = "order:created",
+    OrderCancelled = "order:cancelled",
 }
 
