1a41010437abcd1b4de42a2d4d7a6703422cbb6e
banyarthein <banyarthein@gmail.com>
2021-03-05 22:03:02 +0800

    Ticket created and updated Listener

---
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -0,0 +1,25 @@
+import {Message} from "node-nats-streaming";
+import {Subjects, Listener, TicketUpdatedEvent} from "@ticketingbnt/common";
+import {Ticket} from "../../models/ticket";
+import {queueGroupName as qGroupName} from "./queue-group-name";
+
+export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
+    subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+    queueGroupName = qGroupName;
+
+    async onMessage(data: TicketUpdatedEvent["data"], msg: Message) {
+        const ticket = await Ticket.findById(data.id);
+
+        if(!ticket)
+        {
+            throw new Error("Ticket not found");            
+        }
+
+        const {title, price} = data;
+        ticket.set ({title, price});
+        await ticket.save();
+        
+        msg.ack();
+
+    }
+}
--- orders/src/index.ts
+++ orders/src/index.ts
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
 import { app } from "./app";
+import { TicketCreatedListener } from "./events/listeners/ticket-created-listener";
+import { TicketUpdatedListener } from "./events/listeners/ticket-updated-listener";
 import { natsWrapper } from "./nats-wrapper";
+}
 
 const start = async () => {
 if (!process.env.JWT_KEY) {
@@ -36,7 +39,9 @@ try {
 
     process.on('STGINT', () => natsWrapper.client.close());
     process.on('SIGTERM', () => natsWrapper.client.close());
-    
+
+    new TicketCreatedListener(natsWrapper.client).listen();
+    new TicketUpdatedListener(natsWrapper.client).listen();
 
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
